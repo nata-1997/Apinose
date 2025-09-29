b21d055e24e4ac9cd0b8ba5595c2e62a1c13c6c6
nata-1997 <natalia.carmona5843@uco.net.co>
2025-09-28 21:36:54 -0500

    Carga inicial
    Carga Inicial de código en Github

---
+++ ApiNose/src/main/java/co/edu/uco/nose/crosscuting/helper/ObjectHelper.java
@@ -0,0 +1,16 @@
+package co.edu.uco.nose.crosscuting.helper;
+
+public final class ObjectHelper {
+	
+	private ObjectHelper() {
+	}
+	
+	public static <O> boolean isNull(O object) {
+		return object == null; 
+	}
+	
+	public static <O> O getDefault(final O object, final O defaultValue) {
+		return isNull(object)? defaultValue : object;
+	}
+
+}
---
+++ ApiNose/src/main/java/co/edu/uco/nose/crosscuting/helper/TextHelper.java
@@ -0,0 +1,23 @@
+package co.edu.uco.nose.crosscuting.helper;
+
+public final class TextHelper {
+	// Constante para representar una cadena vacía
+	private static final String EMPTY = "";
+	// Constructor privado para evitar la instanciación
+	private TextHelper() {
+	}
+	// Devuelve una cadena vacía como valor predeterminado
+	public static String getDefault() {
+		return EMPTY;
+	}
+	// Devuelve el valor proporcionado o la cadena vacía si es nulo
+	public static String getDefault(final String value) {
+		return ObjectHelper.getDefault(value, getDefault());
+	}
+	// Devuelve el valor proporcionado o la cadena vacía si es nulo, y elimina los espacios en blanco al inicio y al final
+	public static String getDefaultWithTrim(final String value) {
+		return getDefault(value).trim();
+	}
+	
+
+}
